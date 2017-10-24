d2088417eb2eec80ff4f4ad72a98aeb45fbdb8fe
kun <oiooj@qq.com>
2017-10-24 10:20:33 +0800

    update circle config

--- circle-test.sh
+++ circle-test.sh
@@ -4,7 +4,7 @@ go version
 
 
 # install gdm
-go get github.com/sparrc/gdm && cd ~/.go_workspace/src/github.com/sparrc/gdm && go build -v && sudo cp gdm /usr/local/bin/ 
-cd ${HOME}/.go_workspace/src/github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME && gdm restore
+go get github.com/sparrc/gdm && cd /go/src/github.com/sparrc/gdm && go build -v && sudo cp gdm /usr/local/bin/ 
+cd /go/src/github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME && gdm restore
 
 go test -timeout 60s -v ./...
